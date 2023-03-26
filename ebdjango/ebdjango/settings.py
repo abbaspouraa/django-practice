f7299832e5b47ec12b4895f2aee2d9fc5c4b1d2e
Amir Abbaspour <83620288+abbaspouraa@users.noreply.github.com>
2023-03-26 13:07:10 -0600

    feat(models): add questions and choices

--- ebdjango/ebdjango/settings.py
+++ ebdjango/ebdjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'django-env.eba-mi3fdaqu.us-west-2.elasticbeanstal
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
